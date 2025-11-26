a2866bd0b671f59575cc27a8ce03ab2cc5b3a4e4
FrancescoCapez <checcocap08@gmail.com>
2025-11-26 10:50:11 +0100

    aggiunto file script di stile

---
+++ script.js
@@ -0,0 +1,11 @@
+function elabora(){
+    let a = document.getElementById("inNumber").value;
+    alert(a);
+}
+function annulla(){
+    document.getElementById("inNumber").value=" ";
+}
+function rgb() {
+
+    document.getElementById("inNumber").value=" ";
+}
